2a4c808a9df81786d82910059debbb79873d502f
David Torres <angeldtm09@gmail.com>
2025-05-12 17:11:04 -0700

    Test

--- mongo-tools/scripts/install-mongo-tools.sh
+++ mongo-tools/scripts/install-mongo-tools.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update && apt-get install -y gnupg curl ca-certificates
 
-curl -fsSL https://pgp.mongodb.com/server-6.0.asc | gpg --dearmor -o /usr/share/keyrings/mongodb-server-6.0.gpg
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | gpg --batch --dearmor -o /usr/share/keyrings/mongodb-server-6.0.gpg
 
 echo "deb [ arch=amd64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-6.0.list
 
